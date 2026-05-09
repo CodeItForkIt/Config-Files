fa9d65de322df6b259810d220dc202e58ce73873
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-05-09 08:52:55 -0700

    feat: sync configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+  end,
+})
+require("lspconfig").taplo.setup({})
