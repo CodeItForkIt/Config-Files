05745b77cc5dc3f2d778385aa54da326ca8f0c4d
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-05-24 09:51:43 -0700

    /home/autometalogolex/.config/hypr/keybindings.lua

--- hypr/keybindings.lua
+++ hypr/keybindings.lua
@@ -19,7 +19,7 @@
 --    hl.dsp.exit()
 --    mouse binds use { mouse = true }
 -- ============================================================
-
+local hyprspace = require("edit_here.Hyprspace.Hyprspace")
 local home = os.getenv("HOME")
 local scrPath = home .. "/.local/lib/hyde"
 
@@ -32,7 +32,9 @@ local BROWSER = "vivaldi"
 --  WINDOW MANAGEMENT
 -- ============================================================
 hl.bind("SUPER + Tab", hl.dsp.exec_cmd("rofi -show drun"))
-hl.bind("SHIFT + Tab", hl.plugin.Hyprspace.overview("toggle"))
+hl.bind("SHIFT + Tab", function()
+	hyprspace.toggle()
+end)
 hl.bind("SUPER + Q", hl.dsp.exec_cmd(scrPath .. "/dontkillsteam.sh"), { desc = "close focused window" })
 hl.bind("ALT + F4", hl.dsp.exec_cmd(scrPath .. "/dontkillsteam.sh"), { desc = "close focused window" })
 hl.bind("SUPER + Delete", hl.dsp.exit(), { desc = "kill hyprland session" })
