befb5ab7eccd9512eade32de21234788eb4c17dc
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-07-29 19:42:33 -0700

    config updates

--- hypr/windowrules.lua
+++ hypr/windowrules.lua
@@ -281,7 +281,16 @@ hl.window_rule({
 -- ============================================================
 --  LAYER RULES
 -- ============================================================
-
+hl.layer_rule({
+	name = "noctalia",
+	match = {
+		namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+	},
+	no_anim = true,
+	ignore_alpha = 0.5,
+	blur = true,
+	blur_popups = true,
+})
 hl.layer_rule({
 	name = "lr-rofi",
 	match = { namespace = "rofi" },
