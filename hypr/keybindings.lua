fc29560ac1f74eb4e92eb9bfa7393128929504ef
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-08-21 11:13:03 -0700

    cfg-watch: update hypr/keybindings.lua (2026-08-21 11:13:03)

--- hypr/keybindings.lua
+++ hypr/keybindings.lua
@@ -82,7 +82,7 @@ hl.config({
 
 			gap_size = 20,
 			bg_color = 0xff26233a,
-			border_size = 2,
+			border_size = 0,
 			exit_on_hovered = false,
 			warp_on_move_window = 1,
 			close_overview_on_reload = false,
