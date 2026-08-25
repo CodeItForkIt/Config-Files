565f13cabb7247b134f3efe2cbcf41d0dd6e06de
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-08-24 19:36:43 -0700

    cfg-watch: update hypr/plugins.lua (2026-08-24 19:36:43)

--- hypr/plugins.lua
+++ hypr/plugins.lua
@@ -168,7 +168,10 @@ if hl.plugin.hyprglass then
 	hg.config({ layers = { enabled = true } })
 
 	-- Each call whitelists the namespace and optionally configures it
-	hg.layer("waybar", { preset = "subtle", mask_threshold = 0.05 })
+	hg.layer(
+		"^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+		{ preset = "subtle", mask_threshold = 0.05 }
+	)
 	hg.layer("noctalia")
 	hg.layer("quickshell:bezel", { preset = "ui", mask_threshold = 0.3 })
 	hg.layer("debug-panel", { exclude = true })
