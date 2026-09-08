9c308c7f75722876a4b55bafd480d4465a8f714b
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-09-08 11:13:43 -0700

    cfg-watch: update 3 files (2026-09-08 11:13:43)

--- hypr/scripts/hyprpanel.sh
+++ hypr/scripts/hyprpanel.sh
@@ -11,3 +11,8 @@ test-burst-2-1788891206753876873
 test-burst-3-1788891206955130802
 test-burst-4-1788891207156554114
 test-burst-5-1788891207357883478
+burst-test-1
+burst-test-2
+burst-test-3
+burst-test-4
+burst-test-5
