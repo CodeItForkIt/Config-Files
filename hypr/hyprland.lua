29e6ed78502055d09dee9f2bdad9c5c31ba4945b
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-09-09 15:08:22 -0700

    cfg-watch: update 2 files (2026-09-09 15:08:22)

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -348,3 +348,8 @@ Plugin = {
 
 -- [hyprconf-gui] load GUI-managed overrides last, every reload
 dofile("/home/autometalogolex/.config/hypr/overrides.lua")
+
+
+-- >>> HYPRLAND VISUAL EDITOR (HVE) <<<
+pcall(function() dofile(os.getenv("HOME") .. "/.cache/noctalia/HVE/overlay.lua") end)
+-- <<< HYPRLAND VISUAL EDITOR (HVE) <<<
