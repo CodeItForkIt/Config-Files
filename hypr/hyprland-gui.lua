310f8f6501050ac3cefce5bf7e4d19ef086bd24e
Jeremiah Huebsch <jeremiahhuebsch@icloud.com>
2026-09-08 18:30:04 -0700

    cfg-watch: update 3 files (2026-09-08 18:30:04)

--- hypr/hyprland-gui.lua
+++ hypr/hyprland-gui.lua
@@ -9,6 +9,8 @@ hl.config({
     cursor = {
         no_warps = true,
         persistent_warps = true,
+        warp_on_change_workspace = 0,
+        zoom_factor = 1.1,
     },
     decoration = {
         dim_inactive = true,
